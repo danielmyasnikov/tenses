725b35fe2d5978f4a6965224352f7f50945d2b38
Daniel Myasnikov <myasnikovdaniil@gmail.com>
2014-05-12 20:42:42 +1000

    initial commit

--- lib/tenses.rb
+++ lib/tenses.rb
@@ -1,62 +1,71 @@
 require "tenses/version"
 
-OPTIONS = {
-  :tense =>     [:present,    :past,       :future],
-  :person =>    [:third,      :first,      :second],
-  :plurality => [:singular,   :plural],
-  :diathesis => [:active,     :passive],
-  :mood =>      [:indicative, :imperative, :subjunctive],
-  :aspect =>    [:habitual,   :perfective, :habitual, :progressive, :perfect, :prospective]  
-}
-
-DEFAULTS = {
-  :tense =>     :present,
-  :person =>    :third,
-  :plurality => :singular,
-  :diathesis => :active,
-  :mood =>      :indicative,
-  :aspect =>    :habitual  
-}
-
-
 
 String.class_eval do
 
-  OPTIONS.each do |key, value|
-    value.each do |my_method|
-      # 1. Excluded keys => should be with default values
-      # 2. 
-      # 3. 
+  OPTIONS = {
+    :tense =>     [:present,    :past,       :future],
+    :person =>    [:third,      :first,      :second],
+    :plurality => [:singular,   :plural],
+    :diathesis => [:active,     :passive],
+    :mood =>      [:indicative, :imperative, :subjunctive],
+    :aspect =>    [:habitual,   :perfective, :progressive, :perfect, :prospective]
+  }
+
+  DEFAULTS = {
+    :tense =>     :present,
+    :person =>    :third,
+    :plurality => :singular,
+    :diathesis => :active,
+    :mood =>      :indicative,
+    :aspect =>    :habitual  
+  }
+
+  # Verbs::Conjugator.conjugate self.to_sym, 
+  #   :tense      => :past,
+  #   :person     => :second, 
+  #   :plurality  => :singular, 
+  #   :aspect     => :perfective
+
+  
+
+  
+  def get_methods
+    OPTIONS.values.map{ |v| v + [nil]}.inject(&:product).map(&:flatten).map(&:compact)
+  end
+
+  def methods_to_string(methods)
+    string_methods = []
+    methods.each do |method|
+      string_methods << method.join("_")
     end
+    string_methods
   end
 
-  def to_past
-    Verbs::Conjugator.conjugate self.to_sym, 
-      :tense      => :past,
-      :person     => :second, 
-      :plurality  => :singular, 
-      :aspect     => :perfective
+  def find_excluded_keys(method)
+    method.symbols - DEFAULT.keys
   end
 
-protected
-  def convert
-
+  def symbols(method)
+    method.split("_").map(&:to_sym)
   end
+
+  def find_key_by_value(symbols)
+    excluded_keys = []
+    OPTIONS.each do |key, value|
+      excluded_keys << key if (symbols & value).empty?
+    end
+    excluded_keys
+  end
+
+  def get_defaults(excluded_keys)
+    default_params = {}
+    excluded_keys.each do |ek|
+      default_params.merge!(ek.to_sym => DEFAULTS[ek])
+    end
+    default_params
+  end
+
 end
 
-def convert(hash)
-  hash.each do |k, v|
-    keys = hash.keys
-    keys = keys - [k]
-    keys.each do |key|
-      v.product(hash[key]).each do |item|
-        p item.join("_")
-        excluded_keys = keys - [k, key]
-        default_params = {}
-        excluded_keys.each do |ek|
-          p default_params.merge!(ek.to_sym => DEFAULTS[ek])
-        end
-      end
-    end
-  end
-end
+
